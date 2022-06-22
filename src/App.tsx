02ad1ddbfd1595f31fb602b3c5a01a4587428d95
AffonsoMonteiro <afonsomonteiro_@hotmail.com>
2022-06-22 17:16:02 -0300

    create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,30 @@
 import { gql, useQuery } from '@apollo/client'
 import { useEffect, useState } from 'react'
-import { client } from './lib/apollo'
 
-const GET_LESSONS_QUERY = gql `
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
+import { Event } from './pages/Event'
 
-interface LessonProps {
-  id: string;
-  title: string;
-}
+// const GET_LESSONS_QUERY = gql `
+//   query {
+//     lessons {
+//       id
+//       title
+//     }
+//   }
+// `
+
+// interface LessonProps {
+//   id: string;
+//   title: string;
+// }
 
 function App() {
 
-  const { data } = useQuery<{lessons: LessonProps[]}>(GET_LESSONS_QUERY)
+  // const { data } = useQuery<{lessons: LessonProps[]}>(GET_LESSONS_QUERY)
 
   return (
-    <ul>
-        {data?.lessons.map(lesson => (
-          <li key={lesson.id}> {lesson.title} </li>
-        ))}
-    </ul>
+    <div>
+      <Event />
+    </div>
   )
 }
 
