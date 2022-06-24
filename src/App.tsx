ccd6bc9aa5ebc80367b2598a0f0be91bf60aae5e
AffonsoMonteiro <afonsomonteiro_@hotmail.com>
2022-06-24 13:34:28 -0300

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import { gql, useQuery } from '@apollo/client'
-import { useEffect, useState } from 'react'
-
-import { Event } from './pages/Event'
-
-// const GET_LESSONS_QUERY = gql `
-//   query {
-//     lessons {
-//       id
-//       title
-//     }
-//   }
-// `
-
+import { ApolloProvider } from '@apollo/client'
+import { BrowserRouter } from 'react-router-dom'
+import { client } from './lib/apollo'
+import { Router } from './Router'
 
 function App() {
 
-  // const { data } = useQuery<{lessons: LessonProps[]}>(GET_LESSONS_QUERY)
-
   return (
-    <div>
-      <Event />
-    </div>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
